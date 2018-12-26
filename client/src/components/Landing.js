1a9c9c9562ee2170aa12508021f45819a2f096e8
Alejandro Harb <alejandro@Alejandros-Mac.local>
2018-12-26 12:18:11 -0600

    added build for deployment to heroku

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Landing = () => {
+  return(
+    <div style={{ textAlign: "center"}}>
+      <h1>
+        MyScope!
+      </h1>
+      Find all companies and their info here!
+    </div>
+  )
+};
+
+export default Landing;
--- index.js
+++ index.js
@@ -21,5 +21,16 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+  //express will serve production assets
+  app.use(express.static('client/build'));
+  //Express will serve up the index.html file if it doesn't recognize the route
+  const path = require('path');
+  //catch all http requests that don't match up to an asset or route, and serve back index to pass on to client side.
+  app.get('*', (req, res) =>  {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+};
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
